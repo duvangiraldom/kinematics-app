96bbb252084438c7e4f38983e2ecfdcba4c775b1
duvangiraldom <44323608+duvangiraldom@users.noreply.github.com>
2019-07-06 15:00:19 -0500

    Primera version despues de un mes

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Rigidbody cannonballInstance;
+
+    [SerializeField]
+    [Range(10f, 80f)]
+    private float angle = 45f;
+
+
+
+    private void Update()
+    {
+        //Touch touch = Input.GetTouch(0); touch.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           FireCannonAtPoint(target.transform.position);
+        }
+    }
+
+    private void FireCannonAtPoint(Vector3 point)
+    {
+        Vector3 velocity = BallisticVelocity(point, angle);
+        Debug.Log("Disparando a" + point + " con Velocidad " + velocity);
+        cannonballInstance.transform.position = transform.position;
+        cannonballInstance.velocity = velocity;
+    }
+
+    private Vector3 BallisticVelocity(Vector3 destination, float angle)
+    {
+        Vector3 dir = destination - transform.position; // Obtener la Direccion del Target
+        float height = dir.y; // Obtener la altura (Diferencia de altura)
+        dir.y = 0; // retain only the horizontal difference
+        float dist = dir.magnitude; // get Direccion horizontal
+        float a = angle * Mathf.Deg2Rad; // Convierte el angulo en radianes
+        dir.y = dist * Mathf.Tan(a); // establecer dir en el ángulo de elevación.
+        dist += height / Mathf.Tan(a); // Corrección por pequeñas diferencias de altura.
+
+        // Calculate the velocity magnitude
+        float velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+        return velocity * dir.normalized; // Return a normalized vector.
+    }
+
+}
---
+++ Assets/Scripts/FlashCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashCamera : MonoBehaviour
+{
+    private GameObject CanvasFlash;
+    private GameObject btnON;
+    private GameObject btnOFF;
+
+    void Start()
+    {
+        Vuforia.CameraDevice.Instance.SetFlashTorchMode(false);
+        CanvasFlash = GameObject.Find("flashLight");
+        CanvasFlash.SetActive(true);
+        btnON = GameObject.Find("btnOn");
+        btnON.SetActive(false);
+        btnOFF = GameObject.Find("btnOff");
+        btnON.SetActive(true);
+    }
+
+    public void FlashOn()
+    {
+        Vuforia.CameraDevice.Instance.SetFlashTorchMode(true);
+        btnOFF.SetActive(true);
+        btnON.SetActive(false);
+    }
+
+    public void FlashOff()
+    {
+        Vuforia.CameraDevice.Instance.SetFlashTorchMode(false);
+        btnOFF.SetActive(false);
+        btnON.SetActive(true);
+    }
+
+}
