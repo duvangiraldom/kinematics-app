3ac5f2d6415ee40811aa0abd9a9a5aab763f3d55
duvangiraldom <44323608+duvangiraldom@users.noreply.github.com>
2019-09-06 17:11:40 -0500

    Modificación de interfáz a m´´as amigable

---
+++ Assets/Scripts/InitialVelocity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *Clase para controlar el flash del dispositivo 
+ * @author Duvan Ignacio Giraldo Montoya
+*/
+public class InitialVelocity : MonoBehaviour
+{
+    public GameObject useInitialVelocity;
+    public GameObject useInitialAngle;
+    public GameObject inputInitialVelocity;
+    public GameObject inputInitialAngle;
+
+    void Start()
+    {
+        useInitialVelocity.SetActive(true);
+        inputInitialVelocity.SetActive(false);
+        useInitialAngle.SetActive(false);
+        inputInitialAngle.SetActive(true);
+
+    }
+
+    public void useInitialVelocityController()
+    {
+        useInitialVelocity.SetActive(false);
+        inputInitialVelocity.SetActive(true);
+        useInitialAngle.SetActive(true);
+        inputInitialAngle.SetActive(false);
+    }
+
+    public void useInitialAngleController()
+    {
+        useInitialVelocity.SetActive(true);
+        inputInitialVelocity.SetActive(false);
+        useInitialAngle.SetActive(false);
+        inputInitialAngle.SetActive(true);
+    }
+
+}
