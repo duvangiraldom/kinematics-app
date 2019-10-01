77961d9f4aff3aba74b128e27ac4c5a5f0a839c0
duvangiraldom <44323608+duvangiraldom@users.noreply.github.com>
2019-10-01 13:01:05 -0500

    Se agrega info shot completo a ParabolicShotScene

---
+++ Assets/Scripts/ProjectileRect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileRect : MonoBehaviour
+{
+    // Creates a line renderer that follows a Sin() function
+    // and animates it.
+
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 100;
+
+    void Start()
+    {
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = lengthOfLineRenderer;
+    }
+
+    void Update()
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        var t = Time.time;
+        for (int i = 0; i < lengthOfLineRenderer; i++)
+        {
+            lineRenderer.SetWidth(1, 5);
+
+            //lineRenderer.SetPosition(i, new Vector3(i * 0.5f, 100, 0.0f));
+        }
+    }
+}
