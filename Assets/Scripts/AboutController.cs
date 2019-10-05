7b632a93cc0cf3e99b2e257262f528a7f5491640
duvangiraldom <44323608+duvangiraldom@users.noreply.github.com>
2019-10-04 22:46:51 -0500

    Se organiza ventana de about

---
+++ Assets/Scripts/AboutController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AboutController : MonoBehaviour
+{
+    public Text textInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setTextInfoToAbout();
+    }
+
+    public void setTextInfoToAbout()
+    {
+        textInfo.text = "Kinematic app is developed for people start in the world of physics, in the subjects of Uniform Rectilinear Movement, Uniform Accelerated Movement and Kinematics app's main theme the Parabolic Movement.";
+    }
+
+    public void setTextInfoToInfoDownload()
+    {
+        textInfo.text = "To download the Kinematics App logo you must enter here, download the image (Marker) and print it; so you can scan it with the app";
+    }
+
+    public void setTextInfoToMoreInfo()
+    {
+        textInfo.text = "Hablar de la navegabilidad";
+    }
+}
