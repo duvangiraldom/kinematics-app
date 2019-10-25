4e430e5fb7674560110857ff64cb49a18851fe0d
duvangiraldom <44323608+duvangiraldom@users.noreply.github.com>
2019-10-25 14:20:28 -0500

    Se agregan muros para que la pelota no se caiga al vacio

--- Assets/Scripts/Equation/MRUEquation.cs
+++ Assets/Scripts/Equation/MRUEquation.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MRUEquation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    Transform origin;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Text initialVelocityText;
+
+    [SerializeField]
+    Text finalVelocityText;
+
+    [SerializeField]
+    Text distanceText;
+
+
     void Start()
     {
         
@@ -13,6 +29,13 @@ public class MRUEquation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SetDistanceText();
+    }
+
+    public void SetDistanceText()
+    {
+        distanceText.text = Mathf.Sqrt(((origin.position.x - target.position.x) * (origin.position.x - target.position.x))
+                                    + ((origin.position.z - target.position.z) * (origin.position.z - target.position.z))).
+                                    ToString("F");
     }
 }
---
+++ Assets/Scripts/Motion/MUAMotion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ *Clase para el control del movimiento Rectilineo Uniforme 
+ * @author Duvan Ignacio Giraldo Montoya
+*/
+public class MUAMotion : MonoBehaviour
+{
+    public GameObject target;
+    private float speed;
+
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    }
+    public void SetSpeed(string Speed)
+    {
+        if(Speed != null)
+        {
+            speed = Convert.ToSingle(Speed);
+        }
+    }  
+
+}
